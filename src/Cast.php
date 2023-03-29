21ddc93fe4abc0050859d95f3efa42e6472806cf
R8 <60283818+penobit@users.noreply.github.com>
2023-03-29 15:23:41 +0330

    added laravel cast class

---
+++ src/Cast.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Carbon;
+
+class PersianDate implements CastsAttributes {
+    public function __construct(?string $format = null) {
+        $this->format = $format;
+    }
+
+    /**
+     * Cast the given value.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    public function get($model, string $key, $value, array $attributes) {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof \Penobit\PersianDate\PersianDate) {
+            return $value->toCarbon();
+        }
+
+        $date = persianDate(new Carbon($value));
+
+        if($this->format){
+            return $date->format($this->format);
+        }
+
+        return $date;
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    public function set($model, string $key, $value, array $attributes) {
+        if (empty($value)) {
+            return null;
+        }
+
+        return persianDate($value)->toCarbon();
+    }
+}
